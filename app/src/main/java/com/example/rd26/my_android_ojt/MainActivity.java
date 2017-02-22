d9c4ad1339f20e0be7b7045439172aadce2172fe
rd26 <rd26@everywhere.com.tw>
2017-02-22 17:16:49 +0800

    主畫面

--- app/src/main/java/com/example/rd26/my_android_ojt/MainActivity.java
+++ app/src/main/java/com/example/rd26/my_android_ojt/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.rd26.my_android_ojt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // 項目選擇
+    public void itemClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.main_calculator:
+                break;
+            case R.id.main_list_varible:
+                break;
+            case R.id.main_list_database:
+                break;
+            case R.id.main_contact_list:
+                break;
+            case R.id.main_calender:
+                break;
+            case R.id.main_pic_downloader:
+                break;
+        }
     }
 }
